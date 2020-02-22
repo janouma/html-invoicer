04f8d3e05b7ec4e6cfb85c0feeb81cbf8364f68f
Judicaël Anouma <janouma@gmail.com>
2020-02-22 11:40:43 +0100

    update icu data for node 12

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 
 const { execSync } = require('child_process')
 
-const fullIcuPath = require.resolve('./icudt62l.dat')
+const fullIcuPath = require.resolve('./icudt64l.dat')
 const indexPath = require.resolve('./index')
 
 const out = execSync(`NODE_ICU_DATA=${fullIcuPath} node ${indexPath} ${process.argv.slice(2).join(' ')}`)
